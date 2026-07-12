4321de8174bc060359cbb4d1a3fa850544bdc979
Nidhii Warankar <warankarnidhii@gmail.com>
2026-07-12 12:51:05 +0530

    Added JWT authentication middleware and protected routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import eventRoutes from "./routes/eventRoutes";
 import userRoutes from "./routes/userRoutes";
+import { authenticate } from "./middleware/authMiddleware";
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.use(express.json());
 
 app.get("/health", (_req, res) => {
   res.send("Ticket Booking API is running 🚀");
+});
+
+app.get("/profile", authenticate, (req, res) => {
+  res.json({
+    message: "Protected Route",
+    user: (req as any).user,
+  });
 });
 
 app.use("/events", eventRoutes);
---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface JwtPayload {
+  id: number;
+  email: string;
+}
+
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        message: "No token provided",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+
+    (req as any).user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid or expired token",
+    });
+  }
+};
